b3a55c477a21e3a737cad19dae4c981a99e181ca
fufex3934 <fufawakgari174@gmail.com>
2025-07-23 17:22:40 +0300

    implement controller for todos

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,4 +1,36 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { TodoService } from './todo.service';
+import { Todo } from './schemas/todo.schema';
 
 @Controller('todo')
-export class TodoController {}
+export class TodoController {
+  constructor(private readonly todoService: TodoService) {}
+
+  @Post('create')
+  async create(@Body() todo: Todo) {
+    return this.todoService.create(todo);
+  }
+
+  @Get()
+  async findAll() {
+    return this.todoService.findAll();
+  }
+
+  @Put(':id')
+  async update(@Body() todo: Partial<Todo>, @Param('id') id: string) {
+    return this.todoService.update(id, todo);
+  }
+
+  @Delete(':id')
+  async delete(@Param('id') id: string) {
+    return this.todoService.delete(id);
+  }
+}
--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Todo, TodoDocument } from './schemas/todo.schema';
+import { Model } from 'mongoose';
 
 @Injectable()
-export class TodoService {}
+export class TodoService {
+  constructor(
+    @InjectModel(Todo.name)
+    private todoModel: Model<TodoDocument>,
+  ) {}
+
+  async create(todo: Partial<Todo>) {
+    return this.todoModel.create(todo);
+  }
+
+  async findAll() {
+    return this.todoModel.find().exec();
+  }
+
+  async update(id: string, todo: Partial<Todo>) {
+    return this.todoModel.findByIdAndUpdate(id, todo, { new: true });
+  }
+
+  async delete(id: string) {
+    return this.todoModel.findByIdAndDelete(id);
+  }
+}
